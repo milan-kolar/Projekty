1b759845655a57c8038b991be2e2d56fe376e650
Milan Kolář <minosek.kolar@gmail.com>
2022-11-29 22:10:28 +0100

    File reader

---
+++ File reader.py
@@ -0,0 +1,11 @@
+def read_content(path: str) -> list:
+    try:
+        file = open(path, encoding="utf-8")
+    except FileNotFoundError:
+        return(f"File {path} does not exist!")
+    content = file.read()
+    file.close()
+    return(content.split("\n"))
+
+print(read_content("C:/Users/minos/Documents/ŠKOLA/Prog/2022-2023/Python/Projekty/test.txt"))
+print(read_content("C:/Users/minos/Documents/ŠKOLA/Prog/2022-2023/Python/Projekty/poem.txt"))
