23bc77a7695d315f1e12e4ee78e7ccdb77abd7d6
Milan Kolář <minosek.kolar@gmail.com>
2023-01-17 20:27:31 +0100

    Hra sestřelit míček

--- pygame/Sestřelit míček.py
+++ pygame/Sestřelit míček.py
@@ -27,8 +27,7 @@ x = random.randint(0, 640)
 y = random.randint(0, 480)
 ball = pygame.draw.circle(screen, (0, 0, 255), (x, y), 50)
 
-# Draw the new ball and the score
-pygame.draw.circle(screen, (0, 0, 255), ball.center, 50)
+# Draw the score
 score_text = f"Score: {score}"
 score_font = pygame.font.Font(None, 36)
 score_surface = score_font.render(score_text, True, (0, 0, 0))
@@ -60,15 +59,17 @@ while running:
                 else:
                     x = random.randint(0, 640)
                     y = random.randint(0, 480)
-                    ball = pygame.draw.circle(screen, (0, 0, 255), (x, y), 50)
     # Vymažeme předchozí obsah okna
     screen.fill((255, 255, 255))
-    # Vykreslíme nový míček a skóre
-    pygame.draw.circle(screen, (0, 0, 255), ball.center, 50)
-score_text = f"Score: {score}"
-score_font = pygame.font.Font(None, 36)
-score_surface = score_font.render(score_text, True, (0, 0, 0))
-screen.blit(score_surface, (20, 20))
 
-# Update the display
-pygame.display.flip()
+    # Vykreslíme nový míček a skóre
+    ball = pygame.draw.circle(screen, (0, 0, 255), (x,y), 50)
+ 
+    # Draw the score
+    score_text = f"Score: {score}"
+    score_font = pygame.font.Font(None, 36)
+    score_surface = score_font.render(score_text, True, (0, 0, 0))
+    screen.blit(score_surface, (20, 20))
+
+     # Update the display
+    pygame.display.flip()
